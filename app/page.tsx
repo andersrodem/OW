f28019d557dfa5fd8458de259c985d9be92f3efe
Anders Rodem <anders.rodem@gmail.com>
2023-02-24 14:34:08 +0100

    optimaliserte

--- app/page.tsx
+++ app/page.tsx
@@ -11,11 +11,11 @@ export default function Home() {
         <div className="">
           <img className="rounded-md pt-10" src="/logo.png" alt="Next.js" />
           <h1 className="text-3xl bold pt-10">Keen på litt Kielland?</h1>
-          <img className="my-10 rounded-md"  src="/Kiellis.png" alt="Next.js" />
-          <img className="my-10 rounded-md"  src="/mann.png" alt="Next.js" />
-          <img className="my-10 rounded-md" src="/Pappa.png" alt="Next.js" />
-          <img className="my-10 rounded-md" src="/bil.png" alt="Next.js" />
-          <img className="my-10 rounded-md" src="/bilde.png" alt="Next.js" />
+          <img className="my-10 rounded-md"  src="/Kiellis.jpg" alt="Next.js" />
+          <img className="my-10 rounded-md"  src="/mann.jpg" alt="Next.js" />
+          <img className="my-10 rounded-md" src="/Pappa.jpg" alt="Next.js" />
+          <img className="my-10 rounded-md" src="/bil.jpg" alt="Next.js" />
+          <img className="my-10 rounded-md" src="/bilde.jpg" alt="Next.js" />
           <button className="bg-blue-500 text-white w-full rounded-md py-3">Subscribe for 1$</button>
           <p className="text-gray-300 text-sm text-center pt-5">Laget med kjærlighet av Erling og Anders</p>
         </div>
